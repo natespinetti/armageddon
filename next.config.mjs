f1b391fb68440b0a7c933b4249ebafeb1cb55d28
Nathan Spinetti <104846459+natespinetti@users.noreply.github.com>
2025-02-26 09:41:56 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
         destination: "/",
         permanent: true,
       },
+      {
+        source: "/blog",
+        destination: "/",
+        permanent: true,
+      },
     ];
   },
 };
